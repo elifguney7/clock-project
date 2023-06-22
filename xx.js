c830343370dc5cbc2706423aeff00bd702218971
elifguney7 <elifguney13@gmail.com>
2023-06-22 16:34:04 +0300

    initial commit

---
+++ xx.js
@@ -0,0 +1,226 @@
+//Pomodro
+let sTime;
+
+function setSTime() { 
+    sTime = document.getElementById("studyTime").value;      
+    if (isNaN(sTime) || sTime < 1 || sTime > 90) {
+        alert("Please select between 1 and 90") ;
+    } else {
+        let sText = "You'll be study " + sTime + " munites.";
+        document.getElementById("responseText").innerHTML = sText;
+    }
+}
+let bTime;
+function setBTime(){
+    bTime = document.getElementById("breakTime").value;
+       
+    if (isNaN(bTime) || bTime < 1 || bTime > 30) {
+        alert("Please select between 1 and 30");
+    } else {
+        let bText = " After that you can relax " + bTime + " munites.";
+        document.getElementById("responseText").innerHTML +=  bText;
+    }
+}
+    
+   
+function createTimer () {
+    setInterval (setAlert, sTime*60000);
+    console.log("test");
+}
+
+function setAlert() {
+  alert("Your done with studying ");
+  console.log(sTime);
+}
+
+// const pomodroStart = document.querySelector(".pomodroButton");
+// let xx = document.querySelector(".pomodroNumber");
+// pomodroStart.addEventListener("click", pomodroExec);
+
+// function pomodroExec() {
+//   let second = 20;
+//   const Pomodro = setInterval(() => {
+//     second--;
+//     xx.innerHTML = `${second} `;
+//     if (second === 0) {
+//       clearInterval(Pomodro);
+//     }
+//   }, 1000);
+// }
+
+const pomodroStart = document.querySelector(".pomodroButton");
+let xx = document.querySelector("#pomodroNumber"); // Updated selector to use #
+pomodroStart.addEventListener("click", pomodroExec);
+
+function pomodroExec() {
+  let second = 20;
+  const Pomodro = setInterval(() => {
+    second--;
+    xx.innerHTML = `${second} `;
+    if (second === 0) {
+      clearInterval(Pomodro);
+    }
+  }, 1000);
+}
+
+
+
+
+
+
+
+
+
+
+//Countdown
+function createCountdown(){    
+    let dayTime = document.getElementById("day").value;    
+    let hourTime = document.getElementById("hour").value;
+    let minuteTime = document.getElementById("minute").value;
+    let secondTime = document.getElementById("second").value;
+    secondTime = secondTime < 10 ? "0" + secondTime : secondTime;
+    minuteTime = minuteTime < 10 ? "0" + minuteTime : minuteTime;
+    hourTime = hourTime < 10 ? "0" + hourTime : hourTime;
+    dayTime = dayTime < 10 ? "0" + dayTime : dayTime;
+
+   
+    
+    pageCountdown.innerHTML = `${dayTime}:${hourTime}:${minuteTime}:${secondTime} `;
+    const countDown = setInterval(()=>{
+        secondTime--;
+        secondTime = secondTime < 10 ? "0" + secondTime : secondTime;
+        pageCountdown.innerHTML = `${dayTime}:${hourTime}:${minuteTime}:${secondTime} `;        
+
+       
+        if(dayTime==0 && hourTime==0 && minuteTime==0 && secondTime==0){           
+            document.getElementById("alertSound").play();           
+            pageCountdown.innerHTML = `00:00:00:00 `;            
+            clearInterval(countDown);
+        }
+
+        if(hourTime == 0 && minuteTime == 0 && secondTime == 0 ){
+            dayTime--;
+            hourTime=24;
+        }
+
+        if(minuteTime == 0 && secondTime == 0 ){
+            hourTime--;
+            minuteTime=60;
+        }
+        
+        if(secondTime == 0 ){            
+            minuteTime--;
+            minuteTime = minuteTime < 10 ? "0" + minuteTime : minuteTime;
+            secondTime=60;
+        }        
+    },1000) 
+}  
+
+
+//Stopwatch
+let hour = "0" + 0;
+let min ="0" + 0;
+let sec ="0" + 0;
+let ms = "0" + 0;
+let number = 0;
+let startTimer;
+
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+let resetBtn = document.querySelector(".reset");
+let saveBtn = document.querySelector(".save");
+let deleteBtn = document.querySelector(".delete");
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
+saveBtn.addEventListener("click", save);
+deleteBtn.addEventListener("click", deleteR);
+
+saveBtn.classList.add("active");
+deleteBtn.classList.add("active");
+
+
+function start() {   
+
+    startBtn.classList.add("active");
+    stopBtn.classList.remove("stopActive");
+    saveBtn.classList.remove("active");
+    deleteBtn.classList.remove("active");
+
+
+    startTimer = setInterval(()=>{ 
+        ms++;
+        ms = ms < 10 ? "0" + ms : ms;
+
+        if(ms == 100){            
+            sec++;
+            sec = sec < 10 ? "0" + sec : sec;
+            ms = "0" + 0;        
+        }
+
+        if(sec == 60){            
+            min++;           
+            min = min < 10 ? "0" + min : min;
+            sec = "0" + 0;            
+        }
+        if(min == 60){
+            hour++;
+            hour = hour < 10 ? "0" + hour : hour;
+            min = "0" + 0;          
+        }
+        putValue();
+    }, 10);
+}
+
+function stop() {
+    startBtn.classList.remove("active");
+    stopBtn.classList.add("stopActive");    
+    clearInterval(startTimer);
+}
+
+function reset() {    
+    startBtn.classList.remove("active");
+    stopBtn.classList.remove("stopActive");
+    saveBtn.classList.add("active");    
+    clearInterval(startTimer);
+    hour = min = sec = ms = "0" + 0;
+    putValue();
+}
+
+
+function save (){
+    number++;
+    console.log(number);
+    const saveScreen = document.querySelector(".saveReport");
+
+    const saveLine = document.createElement('div');
+    saveLine.setAttribute('id','reportLine');
+    
+
+    const listNumber = document.createElement('span');
+    listNumber.textContent = number + "- ";
+
+    const savedTime = document.createElement('span');
+    savedTime.textContent = hour + ":" + min + ":" + sec + ":" + ms;
+
+    saveLine.appendChild(listNumber);
+    saveLine.appendChild(savedTime);    
+
+    saveScreen.appendChild(saveLine);
+}
+
+function deleteR() {
+    for(i=0 ; i<number; i++){
+        const deleted = document.getElementById("reportLine");
+        deleted.remove();
+    }
+    number = 0;
+}
+
+function putValue() {
+    document.querySelector(".millisecond").innerText = ms;
+    document.querySelector(".second").innerText = sec;
+    document.querySelector(".minute").innerText = min;
+    document.querySelector(".hour").innerText = hour;
+}
