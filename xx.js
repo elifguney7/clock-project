1dda2abddf194c0d4cc6b07751f46999646b37f4
elifguney7 <elifguney13@gmail.com>
2023-07-02 22:29:34 +0300

    add countdown timer to pomodro page

--- xx.js
+++ xx.js
@@ -1,37 +1,138 @@
 //Pomodro
+
 let sTime;
-
-function setSTime() { 
-    sTime = document.getElementById("studyTime").value;      
-    if (isNaN(sTime) || sTime < 1 || sTime > 90) {
-        alert("Please select between 1 and 90") ;
-    } else {
-        let sText = "You'll be study " + sTime + " munites.";
-        document.getElementById("responseText").innerHTML = sText;
-    }
-}
 let bTime;
-function setBTime(){
-    bTime = document.getElementById("breakTime").value;
-       
-    if (isNaN(bTime) || bTime < 1 || bTime > 30) {
-        alert("Please select between 1 and 30");
-    } else {
-        let bText = " After that you can relax " + bTime + " munites.";
-        document.getElementById("responseText").innerHTML +=  bText;
-    }
-}
-    
-   
-function createTimer () {
-    setInterval (setAlert, sTime*60000);
-    console.log("test");
+let timerInterval;
+let isStudyTime = true;
+
+function setSTime() {
+  sTime = document.getElementById("studyTime").value;
+
+  if (isNaN(sTime) || sTime < 1 || sTime > 90) {
+    alert("Please select between 1 and 90");
+  } else {
+    let sText = "You'll study for " + sTime + " minutes.";
+    document.getElementById("responseText").innerHTML = sText;
+  }
 }
 
-function setAlert() {
-  alert("Your done with studying ");
-  console.log(sTime);
+function setBTime() {
+  bTime = document.getElementById("breakTime").value;
+
+  if (isNaN(bTime) || bTime < 1 || bTime > 30) {
+    alert("Please select between 1 and 30");
+  } else {
+    let bText = " After that, you can relax for " + bTime + " minutes.";
+    document.getElementById("responseText").innerHTML += bText;
+  }
 }
+
+const FULL_DASH_ARRAY = 283;
+
+const COLOR_CODES = {
+  study: {
+    color: "pink"
+  },
+  break: {
+    color: "blue"
+  }
+};
+
+let remainingPathColor;
+
+document.addEventListener('DOMContentLoaded', function () {
+  let timeLeft = 0;
+
+  document.getElementById("app").innerHTML = `
+    <div class="pcontainer">
+      <button type="button" class="pomodoroButton">Start</button>
+      <div class="base-timer">
+        <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <g class="base-timer__circle">
+            <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+            <path
+              id="base-timer-path-remaining"
+              stroke-dasharray="${FULL_DASH_ARRAY}"
+              class="base-timer__path-remaining ${remainingPathColor}"
+              d="
+                M 50, 50
+                m -45, 0
+                a 45,45 0 1,0 90,0
+                a 45,45 0 1,0 -90,0
+              "
+            ></path>
+          </g>
+        </svg>
+        <span id="base-timer-label" class="base-timer__label">
+          ${formatTimeLeft(timeLeft)}
+        </span>
+      </div>
+    </div>
+  `;
+
+  const startButton = document.querySelector('.pomodoroButton');
+  startButton.addEventListener('click', startPomodoro);
+
+  function startPomodoro() {
+    if (isStudyTime) {
+      startTimer(sTime * 60, COLOR_CODES.study.color);
+    } else {
+      startTimer(bTime * 60, COLOR_CODES.break.color);
+    }
+  }
+
+  function startTimer(timeLimit, color) {
+    clearInterval(timerInterval);
+    remainingPathColor = color;
+    document.getElementById("base-timer-path-remaining").style.stroke = remainingPathColor;
+
+    timeLeft = timeLimit;
+    timerInterval = setInterval(() => {
+      timeLeft -= 1;
+      document.getElementById("base-timer-label").innerHTML = formatTimeLeft(timeLeft);
+      setCircleDasharray(timeLimit, timeLeft);
+
+      if (timeLeft === 0) {
+        clearInterval(timerInterval);
+        onTimesUp();
+        if (isStudyTime) {
+          isStudyTime = false;
+          startPomodoro();
+        } else {
+          isStudyTime = true;
+          startPomodoro();
+        }
+      }
+    }, 1000);
+  }
+
+  function formatTimeLeft(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+  }
+
+  function calculateTimeFraction(timeLimit, timeLeft) {
+    const rawTimeFraction = timeLeft / timeLimit;
+    return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
+  }
+
+  function setCircleDasharray(timeLimit, timeLeft) {
+    const circleDasharray = `${(calculateTimeFraction(timeLimit, timeLeft) * FULL_DASH_ARRAY).toFixed(0)} ${FULL_DASH_ARRAY}`;
+    document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
+  }
+
+  function onTimesUp() {
+    remainingPathColor = isStudyTime ? COLOR_CODES.study.color : COLOR_CODES.break.color;
+    document.getElementById("base-timer-path-remaining").style.stroke = remainingPathColor;
+  }
+});
+
+
+
 
 
 
